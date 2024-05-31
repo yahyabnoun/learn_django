0cf58604c8150b48633517542545e7d69f5f1082
Yahya bnoun <yahyabnoun@gmail.com>
2024-05-31 11:54:26 +0100

    products

--- products/models.py
+++ products/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
-    content = models.TextField()
+
+    categoriesChoises = [
+        ("phone","phone"),
+        ("computer","computer"),
+    ]
+
+    name = models.CharField(max_length=50,verbose_name="title")
+    content = models.TextField(null=True,blank=True,verbose_name="description")
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to='photo/%y/m/%d')
     active = models.BooleanField(default=True)
+    categories = models.CharField(max_length=150, null=True, blank=True, choices=categoriesChoises)
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Product"
+        ordering = ["-name"]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Product
+
 
 # Create your views here.
+def products(request):
+    return render(request, "pages/products/products.html", {"products":Product.objects.all()})
