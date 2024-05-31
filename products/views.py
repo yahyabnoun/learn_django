f1ead6ef41a226d68c44e5d139b2bd806d0dd5ef
Yahya bnoun <yahyabnoun@gmail.com>
2024-05-31 16:27:27 +0100

    modele

--- products/views.py
+++ products/views.py
@@ -4,7 +4,10 @@ from .models import Product
 
 # Create your views here.
 def products(request):
-    return render(request, "pages/products/products.html", {"products":Product.objects.all()})
+    objectproducts = Product.objects
+    # products = {"products":objectproducts.filter(name__contains="x")}
+    products = {"products":objectproducts.filter(name__contains="x")}
+    return render(request, "pages/products/products.html", products)
 
 def product(request):
     return render(request, "pages/products/product.html", {"product":Product.objects.get(id=1)})
