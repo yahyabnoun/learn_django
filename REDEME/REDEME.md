cdcbda547ae79533436577c5319ce0ef0cb7f1bf
Yahya bnoun <yahyabnoun@gmail.com>
2024-05-31 09:25:29 +0100

    #

--- REDEME/REDEME.md
+++ REDEME/REDEME.md
@@ -4,3 +4,7 @@ python manage.py startapp products
 py manage.py runserver 8080
 
 python manage.py collectstatic
+
+python manage.py makemigrations
+
+python manage.py migrate 
--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path ,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('products/', include('products.urls')),
+    # path('products/', include('products.urls')),
 ]
